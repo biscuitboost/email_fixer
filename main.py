be16c314c3b80ec60ad85be6c80c185349db283f
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-03-30 16:58:12 +0100

    update

--- main.py
+++ main.py
@@ -48,13 +48,11 @@ st.header("Globalize Text")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
-                will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
+    st.markdown("text here \
+                and here")
 
 with col2:
-    st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
+    st.markdown("more text here")    
 
 st.markdown("## Enter Your Email To Convert")
 
