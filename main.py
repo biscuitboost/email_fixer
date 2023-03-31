654e3cb3a44cd6bfe35bb2396690bd34d63955f9
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-03-31 11:23:54 +0100

    update

--- main.py
+++ main.py
@@ -86,13 +86,14 @@ st.button("*See An Example*",
 
 
 if email_input:
-    llm = load_LLM(openai_api_key=openai_api_key)
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
-    formatted_email = llm(prompt_with_email)
-    with st.container():
-        st.markdown("### Your Converted Email:")
-        st.code(formatted_email, language=None)
-        st.balloons()
+    with st.spinner(text="In progress...")
+        llm = load_LLM(openai_api_key=openai_api_key)
+        prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+        formatted_email = llm(prompt_with_email)
+        with st.container():
+            st.markdown("### Your Converted Email:")
+            st.write(formatted_email)
+    st.balloons()
 
 
 col1, col2 = st.columns(2)        
