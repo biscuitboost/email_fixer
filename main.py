21e72efacbf8010b99c4640d7dd8fdfc12933124
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-04-01 19:12:17 +0100

    update

--- main.py
+++ main.py
@@ -65,7 +65,18 @@ st.set_page_config(
 )
 st.header("Email Fixer")
 
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
 
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
 st.markdown("## Enter Your Email To Convert")
 
