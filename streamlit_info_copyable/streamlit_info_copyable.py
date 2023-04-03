832f6652a64bb2c3c9459d685406bb615d2128fe
biscuitboost <107883057+biscuitboost@users.noreply.github.com>
2023-04-03 17:59:16 +0100

    update

--- streamlit_info_copyable/streamlit_info_copyable.py
+++ streamlit_info_copyable/streamlit_info_copyable.py
@@ -7,7 +7,7 @@ if not _RELEASE:  # during development
     _component_func = components.declare_component("streamlit_info_copyable", url="http://localhost:3001")
 else:  # when hosted
     parent_dir = os.path.dirname(os.path.abspath(__file__))
-    _component_func = components.declare_component("streamlit_info_copyable")
+    _component_func = components.declare_component("/app/email_fixer/streamlit_info_copyable")
 
 def st_info_copyable(text: str):
     _component_func(text=text)
